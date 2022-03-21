601072b4295f7c2f5cb6051d9d017feddefbb414
Sanfutty14 <sanfar@psts-sg.com>
2022-03-21 21:11:57 +0530

    code reduced

--- src/components/navlayout/index.js
+++ src/components/navlayout/index.js
@@ -4,35 +4,9 @@ import { AppBar, Box, Toolbar, IconButton, Typography, InputBase, MenuItem, Menu
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import { useAuth0 } from '@auth0/auth0-react';
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-    },
-}));
+const Search = styled('div')(({ theme }) => ({ position: 'relative', borderRadius: theme.shape.borderRadius, backgroundColor: alpha(theme.palette.common.white, 0.15), '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25), }, marginRight: theme.spacing(2), marginLeft: 0, width: '100%', [theme.breakpoints.up('sm')]: {    marginLeft: theme.spacing(3),   width: 'auto', },}));
 const SearchIconWrapper = styled('div')(({ theme }) => ({padding: theme.spacing(0, 2),height: '100%',position: 'absolute',pointerEvents: 'none',display: 'flex',alignItems: 'center',justifyContent: 'center',}));
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
-    },
-}));
+const StyledInputBase = styled(InputBase)(({ theme }) => ({  color: 'inherit','& .MuiInputBase-input': {  padding: theme.spacing(1, 1, 1, 0),   paddingLeft: `calc(1em + ${theme.spacing(4)})`,  transition: theme.transitions.create('width'),  width: '100%',   [theme.breakpoints.up('md')]: {   width: '20ch',   },  },}));
 export default function PrimarySearchAppBar() {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const isMenuOpen = Boolean(anchorEl);
@@ -40,55 +14,11 @@ export default function PrimarySearchAppBar() {
     const handleProfileMenuOpen = (event) => {setAnchorEl(event.currentTarget);};
     const handleMenuClose = () => {setAnchorEl(null);};
     const menuId = 'primary-search-account-menu';
-    const renderMenu = (
-        <Menu anchorEl={anchorEl} anchorOrigin={{vertical: 'top', horizontal: 'right', }} id={menuId}keepMounted
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            placement="bottom-start"
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-            sx={{ mt: 1.5,}}
-        ><MenuItem onClick={handleMenuClose}>{user.name}</MenuItem><MenuItem onClick={() => logout({ returnTo: window.location.origin })}>Logout</MenuItem></Menu>
-    );
+    const renderMenu = (   <Menu anchorEl={anchorEl} anchorOrigin={{vertical: 'top', horizontal: 'right', }} id={menuId} keepMounted
+   transformOrigin={{ vertical: 'top',  horizontal: 'right', }} placement="bottom-start" open={isMenuOpen} onClose={handleMenuClose} sx={{ mt: 1.5,}} ><MenuItem onClick={handleMenuClose}>{user.name}</MenuItem><MenuItem onClick={() => logout({ returnTo: window.location.origin })}>Logout</MenuItem></Menu>);
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ display: { xs: 'none', sm: 'block' } }}
-                    >
-                        Hiring.com
-                    </Typography>
-                    <Search>
-                        <SearchIconWrapper>
-                            <SearchIcon />
-                        </SearchIconWrapper>
-                        <StyledInputBase
-                            placeholder="Search…"
-                            inputProps={{ 'aria-label': 'search' }}
-                        />
-                    </Search>
-                    <Box sx={{ flexGrow: 1 }} />
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <IconButton
-                            size="large"
-                            edge="end"
-                            aria-label="account of current user"
-                            aria-controls={menuId}
-                            aria-haspopup="true"
-                            onClick={handleProfileMenuOpen}
-                            color="inherit"
-                        >
-                            <AccountCircle />
-                        </IconButton>
-                    </Box>
-                </Toolbar>
-            </AppBar>
+            <AppBar position="static"> <Toolbar> <Typography  variant="h6"  noWrap component="div"   sx={{ display: { xs: 'none', sm: 'block' } }} >  Hiring.com </Typography>  <Search><SearchIconWrapper> <SearchIcon /> </SearchIconWrapper> <StyledInputBase   placeholder="Search…"   inputProps={{ 'aria-label': 'search' }} /></Search> <Box sx={{ flexGrow: 1 }} />  <Box sx={{ display: { xs: 'none', md: 'flex' } }}>   <IconButton  size="large" edge="end" aria-label="account of current user"  aria-controls={menuId}  aria-haspopup="true" onClick={handleProfileMenuOpen}  color="inherit" >  <AccountCircle /> </IconButton> </Box> </Toolbar>  </AppBar>
             {renderMenu}
         </Box>
     );
